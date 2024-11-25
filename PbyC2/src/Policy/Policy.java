c0c903b5897a9b4174decd13c302cbd8dedf2f18
sdac-23 <sdac-23@sdac-23-PC>
2024-11-25 12:05:26 +0530

    Policy

---
+++ PbyC2/src/Policy/Policy.java
@@ -0,0 +1,12 @@
+package Policy;
+
+public interface Policy {
+	
+	public double calSal(double sal, double bonus);
+
+	public double calSal(double sal);
+	
+	public double calSal(double sal, double bonus,double tax);
+
+
+}
