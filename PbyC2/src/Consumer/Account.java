3415e14c2d5912bb5605c05873e1b9d11a24d7cf
sdac-23 <sdac-23@sdac-23-PC>
2024-11-25 12:08:17 +0530

    consumer

---
+++ PbyC2/src/Consumer/Account.java
@@ -0,0 +1,23 @@
+package Consumer;
+
+public class Account extends Company.Company{
+
+	@Override
+	public double calSal(double sal) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	@Override
+	public double calSal(double sal, double bonus) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	@Override
+	public double calSal(double sal, double bonus, double tax) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
---
+++ PbyC2/src/Consumer/Manager.java
@@ -0,0 +1,11 @@
+package Consumer;
+
+public class Manager extends Account{
+
+	
+	@Override
+	public double calSal(double sal, double bonus) {
+		// TODO Auto-generated method stub
+		return sal+bonus;
+	}
+}
